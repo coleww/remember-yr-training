605de7ad6b7f9244e3d2a8f73d427fe8700cb2df
Cole Willsea <coleww@gmail.com>
2016-02-29 09:46:51 -0800

    nice tunes yo

--- sound/potentialSequencerModule.js
+++ sound/potentialSequencerModule.js
@@ -12,12 +12,19 @@ module.exports = function (currentSong) {
   return {
     start: function () {
       interval = setInterval(function () {
-        song.instruments.forEach(function (instrument) {
+        Object.keys(song.instruments).forEach(function (k) {
+          var instrument = song.instruments[k]
           var pattern = instrument.patterns[song.current]
           pattern.currentTick++
           if (pattern.currentTick == pattern.probs[pattern.currentVersion].length - 1) {
             pattern.currentTick = 0
             pattern.currentVersion = pick(pattern.nexts[pattern.currentVersion])
+            console.log('WEEEE', pattern.currentVersion)
+            if (instrument.lead) {
+              song.current = pick(song.nexts[song.current])
+              if (!song.current) alert('it is over')
+              console.log('SWITCHY', song.current)
+            }
           }
           if (roll(pattern.probs[pattern.currentVersion][pattern.currentTick])) {
             instrument.play(pattern.notes ? pick(pattern.notes[pattern.currentVersion][pattern.currentTick]) : undefined)
--- sound/sequencer.js
+++ sound/sequencer.js
@@ -1,72 +1,27 @@
-var song = {
-  instruments: [
-  {
-    patterns: {
-      verse: {
-        notes: [
-          [[0], [], [4, 0], []],
-          [[0], [5], [4, 0], [2, 0]]
-        ],
-        probs: [
-          [1, 0, 1, 0],
-          [1, 0.5, 1, 0.75]
-        ],
-        currentVersion: 0,
-        currentTick: 0,
-        nexts: [[0, 0, 1], [1, 1, 1, 0]]
-      },
-      chorus: {
-        notes: [
-          [[0], [2, 0], [4, 5], [6, 6, 7]],
-          [[], [5, 5, 2], [4, 0], [2, 0, 6]]
-        ],
-        probs: [
-          [1, 0.25, 1, 0.25],
-          [0, 0.5, 0.25, 0.75]
-        ],
-        currentVersion: 0,
-        currentTick: 0,
-        nexts: [[0, 0, 1], [1, 1, 1, 0]]
-      },
-      key: {
-        tonic: 'A',
-        scale: 'major'
-      }
-    }
-  }
-  ],
-  current: 'verse',
-  nexts: {
-    verse: ['verse', 'verse', 'chorus'],
-    chorus: ['chorus', null]
-  },
-  bpm: 120
-}
-
+var song = require('../songs/bunker')
+var int2freq = require('int2freq')
 var seqy = require('./potentialSequencerModule')
 module.exports = function (ac, mainVolume) {
-  // var snare = require("dj-snazzy-snare")(ac)
-  // var tom = require("tom-from-space")(ac)
-  // var kick = require("touch-down-dance")(ac)
-  // var hat = require("really-hi-hat")(ac)
-  // var wBass= require("warlock-bass")(ac)
-  // var bBass = require("bubble-bass")(ac)
-  var piano = require("pie-ano")(ac)
-  // var sparkles = require("sparkle-motion")(ac)
-  piano.connect(mainVolume)
-  song.instruments[0].play = function (arg) {
-    piano.update({midiNote: 69 + arg, attack: 0.3, decay: 0.1, sustain: 0.3, release: 0.5, peak: 0.5, mid: 0.3, end: 0.00000001}, ac.currentTime)
-    piano.start(ac.currentTime)
+  var insts = {
+    snare: require("dj-snazzy-snare")(ac),
+    tom: require("tom-from-space")(ac),
+    kick: require("touch-down-dance")(ac),
+    hat: require("really-hi-hat")(ac),
+    wBass: require("warlock-bass")(ac),
+    bBass: require("bubble-bass")(ac),
+    piano: require("pie-ano")(ac),
+    sparkles: require("sparkle-motion")(ac)
   }
+  Object.keys(insts).forEach(function(k) {
+    insts[k].connect(mainVolume)
+    song.instruments[k].play = function (arg) {
+      if (song.instruments[k].melodic) insts[k].update({freq: int2freq(arg, song.key), attack: 0.3, decay: 0.1, sustain: 0.3, release: 0.5, peak: 0.5, mid: 0.3, end: 0.00000001}, ac.currentTime)
+      insts[k].start(ac.currentTime)
+    }
+  })
+
+  song.instruments[0]
   return seqy(song)
-  // snare.connect(mainVolume)
-  // tom.connect(mainVolume)
-  // kick.connect(mainVolume)
-  // hat.connect(mainVolume)
-  // wBass.connect(mainVolume)
-  // bBass.connect(mainVolume)
-  //
-  // sparkles.connect(mainVolume)
 }
 
 
